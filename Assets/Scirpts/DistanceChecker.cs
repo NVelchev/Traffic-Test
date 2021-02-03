0e859fcaef614671f5a447f80bf84800581e2774
Nicholas Velchev <nnvelchev@gmail.com>
2021-02-03 21:16:47 +0200

    Finalized version of the demo project
    FInal Version of the demo

---
+++ Assets/Scirpts/DistanceChecker.cs
@@ -0,0 +1,62 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class DistanceChecker : MonoBehaviour
+{
+    public GameObject RayCastShooter;
+    public vehicleAiController VAIC;
+    public Rigidbody rb;
+    public float xVelocity;
+    public IEnumerator Wait() //just in case
+    {
+        yield return new WaitForSecondsRealtime(2);
+    }
+
+    void FixedUpdate()
+    {
+        LayerMask mask = LayerMask.GetMask("Car");
+        LayerMask Redlight = LayerMask.GetMask("RedLight"); // Future support for red lights
+        LayerMask Pedestrian = LayerMask.GetMask("Pedestrian");
+        RaycastHit hit;
+        xVelocity = rb.velocity.magnitude;
+       
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 6.5f, mask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+            
+            VAIC.totalPower = (xVelocity >= 0.15f) ? Mathf.Lerp(0, -500f, 1) : -0f; // ? Means then , : means else
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 4, Color.white);
+            
+            VAIC.totalPower = 35f;
+        }
+        //Red Light yield
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 3.5f, Redlight))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+            
+            VAIC.totalPower = (xVelocity >= 0.15f) ? Mathf.Lerp(0, -550f, 1) : 0f; // ? Means then , : means else;
+        }
+        // pedestrian yield
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 6.5f, Pedestrian))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+            
+            VAIC.totalPower = (xVelocity >= 0.15f) ? Mathf.Lerp(0, -550f, 1) : 0f; // ? Means then , : means else;
+        }
+    }
+
+
+
+
+}
+
+
+
---
+++ Assets/Scirpts/Traffic AI/vehicle Ai scripts/Scripts/AI/pedestrianSpawner.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class pedestrianSpawner : MonoBehaviour{
+
+
+
+
+    public waypoint[] waypoints;
+
+
+    public GameObject[] pedestrian;
+    public int totalPopulationMax = 20;
+    public float maxDistance = 100f;
+
+    public List<GameObject> spawnedAi ;
+
+    void Awake(){
+       // loadArray();
+        StartCoroutine(managePedestrian());
+        managePopulation();
+    }
+
+    //void loadArray(){
+      //  waypoints = FindObjectsOfType<waypoint>(); doesnt load the waypoints in the correct order, so it's been removed as of now.
+
+    //}
+
+    void spawnPrefab(int index){     
+
+        GameObject i = Instantiate(pedestrian[Random.Range(0,pedestrian.Length)] );
+        i.GetComponent<ThirdPersonUserControl>().currentWaypoint = waypoints[index].GetComponent<waypoint>();
+        i.transform.position = waypoints[index].transform.position;
+        spawnedAi.Add(i);
+        
+    }
+
+    public IEnumerator managePedestrian(){
+
+		while(true){
+			yield return new WaitForSeconds(20);
+            managePopulation();
+		}
+	
+
+    }
+
+    void managePopulation(){
+        for (int i = 0; i < waypoints.Length; i++){
+            if(spawnedAi.Count <= totalPopulationMax)
+                populationLoop(i);
+        }
+
+        for (int i = 0; i < spawnedAi.Count; i++){
+            if(Vector3.Distance(transform.position , spawnedAi[i].transform.position) >= maxDistance){
+                Destroy(spawnedAi[i]);
+                spawnedAi.Remove(spawnedAi[i]);
+            }
+        }
+
+    }
+
+    void populationLoop(int index){
+        if(Vector3.Distance(transform.position , waypoints[index].transform.position) <= maxDistance){
+            spawnPrefab(index);
+        }
+    }
+
+
+}
