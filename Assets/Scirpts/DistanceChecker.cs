f24b7641f74ec60d5d9aa3f0f65ae99da1a397ff
Nicholas Velchev <nnvelchev@gmail.com>
2021-02-03 21:17:41 +0200

    Fixed PackageManager error

--- Assets/Scirpts/DistanceChecker.cs
+++ Assets/Scirpts/DistanceChecker.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using UnityEditor.PackageManager;
+
 using UnityEngine;
 
 public class DistanceChecker : MonoBehaviour
